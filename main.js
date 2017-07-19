2e72f5427a59c32ab4894745915c2030c23f5e40
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-19 09:15:08 -0500

    implemented all features, some difficulties with matching algorithm

--- main.js
+++ main.js
@@ -15,32 +15,60 @@ function getClick() {
 	count++
 	if (count % 2 !== 0) {
 		this.innerText = 'O';
+		this.classList.add('blue');
 		vals1 += this.id;
 	} else {
 		this.innerText = 'X';
+		this.classList.add('red');
 		vals2 += this.id;
 	}
-	evaluateString(vals1, vals2);
+	evaluateString(vals1, 'O');
+	evaluateString(vals2, 'X');
 }
 
-function evaluateString() {
-	if(vals1.length > 3) {
-		vals1.shift();
-	} else if (vals2.length > 3) {
-		vals2.shift();
+function stopClicks() {
+	for(let i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', getClick);
 	}
-	console.log(vals1, vals2);
 }
 
-//  when reset button is pressed
-  //reset count
-  //reset arrs
-  //change all cells to #
-  //clear display
+function evaluateString(str, winner) {
+
+	let toArr = str.split('');//string to array
+	if (toArr.length > 3) {
+		let shifted = toArr.shift();//remove oldest value
+	}
+	let sortedStr = toArr.sort().join('');//sort values, join
+	console.log(sortedStr);
+	if (								//compare
+		sortedStr === '123' ||
+		sortedStr === '456' ||
+		sortedStr === '789' ||
+		sortedStr === '147' ||
+		sortedStr === '258' ||
+		sortedStr === '369' ||
+		sortedStr === '159' ||
+		sortedStr === '357'
+
+		) {
+		button.className = 'shown';
+		display.innerText = winner + ' is the winner.';
+		stopClicks();
+	} else if (count === 9) {
+		button.className = 'shown';
+		display.innerText = 'Tie Game.';
+		stopClicks();
+	}
+
+}
+
 
 for(let i = 0; i < cells.length; i++) {
 	cells[i].addEventListener('click', getClick);
 }
 
+button.addEventListener('click', function() {
+	location.reload();
+});
 
 
